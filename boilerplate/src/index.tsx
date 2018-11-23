847f2e7a3c9819244de6d4f5b134824e76f53075
t_daicho <daicho.t@gmail.com>
2018-11-23 16:46:00 +0900

    boilerplate

---
+++ boilerplate/src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import styles from './index.css';
+
+ReactDOM.render(
+    <div className={styles.sample}>foobarbaz</div>,
+    document.getElementById('app-route')
+);
